e055a17afeedd6d296d2a8e08d46df4cc487a864
varinder94 <60019986+varinder94@users.noreply.github.com>
2020-02-14 14:20:45 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Varinder Kaur");
+		System.out.println("kaurv6@student.douglascollege.ca");
+
+	}
+
+}
